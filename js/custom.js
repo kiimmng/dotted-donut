fc5c3dd6eadddc1e0d92029fb15498b190713177
kiimmng <121073116+kiimmng@users.noreply.github.com>
2023-01-16 02:48:15 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+new fullpage('#fullpage', {
+
+    //options here
+
+    autoScrolling: true,
+    credits: { enabled: false, label: 'Made with fullPage.js', position: 'right' },
+});
+
+//methods
+
+fullpage_api.setAllowScrolling(true);
+
+// 
+
+
